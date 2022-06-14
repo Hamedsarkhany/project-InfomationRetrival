4dfdbfa0e03dc0dffc7053786adb2549795e9c04
Hamedsarkhany <92758067+Hamedsarkhany@users.noreply.github.com>
2022-06-15 00:25:13 +0430

    Add files via upload

---
+++ cral.py
@@ -0,0 +1,44 @@
+from asyncio.windows_events import NULL
+import requests
+from bs4 import BeautifulSoup 
+from newspaper import Article
+from tqdm import tqdm
+import pandas as pd
+
+def scrap_page(page : int):
+    scraped_data = []
+    url_list = []
+
+    while page<=134:
+        page += 1
+         
+        main_page_url = f"https://www.yjc.news/fa/allnews?page={page}"
+      
+         
+        html = requests.get(main_page_url).text
+
+        soup = BeautifulSoup(html, features='lxml')
+
+        links = soup.find_all('h2')
+
+
+        for link in tqdm(links):
+            page_url = 'https://www.yjc.news' + link.a['href']
+            url_list.append(link.a['href'])
+            try:
+                article = Article(page_url)  
+                article.download()
+                article.parse()
+                scraped_data.append({'url' : page_url, 'text' : article.text , 'title' : article.title}) 
+            except:
+                print(f"failed to process page: {page_url}")
+                    
+    df = pd.DataFrame(scraped_data)
+    df.to_csv(f'yjc.csv')
+
+
+if __name__ == '__main__':
+    scrap_page(1)
+    
+
+             
